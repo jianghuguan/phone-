ec6b2fa0ba3a27c43d982cc58b3568f1fb4f0cae
jianghuguan <238134295+jianghuguan@users.noreply.github.com>
2026-04-22 23:52:14 +0800

    Update store.js

--- js/store.js
+++ js/store.js
@@ -1,36 +1,39 @@
-/* eslint-disable */
-// 加上面这行注释可以消除大部分平台的代码语法检查红叉
+window.store = Vue.reactive({
+  currentApp: null,
 
-window.store = window.Vue.reactive({
-    currentApp: null,
-    
-    // 自定义组件的背景链接
-    timeWidgetBg: 'https://images.unsplash.com/photo-1506744626753-1fa44df6231e?w=800&q=80',
-    photoWidgetImg: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?w=400&q=80',
+  installedApps: [
+    { id: 'qq', name: 'QQ', textIcon: 'Q', color: '#e8f4fd' },
+    { id: 'sms', name: '短信', textIcon: '信', color: '#e8fde8' },
+    { id: 'weibo', name: '微博', textIcon: '微', color: '#fde8e8' },
+    { id: 'forum', name: '论坛', textIcon: '论', color: '#f3e8fd' },
+    { id: 'music', name: '音乐', textIcon: '音', color: '#fde8f5' },
+    { id: 'theme', name: '美化', textIcon: '美', color: '#fdfce8' },
+    { id: 'settings', name: '设置', textIcon: '设', color: '#eeeeee' },
+    { id: 'widgetApp', name: '小组件', textIcon: '组', color: '#e8fdfa' }
+  ],
 
-    // App 的配置库
-    installedApps: [
-        { id: 'qq', name: 'QQ', textIcon: 'Q', color: '#e8f4fd' },
-        { id: 'sms', name: '短信', textIcon: '信', color: '#e8fde8' },
-        { id: 'weibo', name: '微博', textIcon: '微', color: '#fde8e8' },
-        { id: 'forum', name: '论坛', textIcon: '论', color: '#f3e8fd' },
-        { id: 'music', name: '音乐', textIcon: '音', color: '#fde8f5' },
-        { id: 'theme', name: '美化', textIcon: '美', color: '#fdfce8' },
-        { id: 'settings', name: '设置', textIcon: '设', color: '#eeeeee' },
-        { id: 'widgetApp', name: '小组件', textIcon: '组', color: '#e8fdfa' }
-    ],
-
-    // 核心机制：桌面的展示列表及网格分布顺序
-    desktopItems: [
-        { type: 'widget', id: 'timeWidget', span: '4 / 2' },
-        { type: 'widget', id: 'photoWidget', span: '2 / 2' },
-        { type: 'app', id: 'qq', span: '1 / 1' },
-        { type: 'app', id: 'sms', span: '1 / 1' },
-        { type: 'app', id: 'weibo', span: '1 / 1' },
-        { type: 'app', id: 'forum', span: '1 / 1' },
-        { type: 'app', id: 'music', span: '1 / 1' },
-        { type: 'app', id: 'theme', span: '1 / 1' },
-        { type: 'app', id: 'settings', span: '1 / 1' },
-        { type: 'app', id: 'widgetApp', span: '1 / 1' }
+  widgetSettings: {
+    timeWeatherBg: '',
+    photoWallImages: [
+      '',
+      '',
+      '',
+      ''
     ]
+  },
+
+  desktopItems: [
+    { id: 'widget-time', type: 'widget', widgetType: 'timeWeather', x: 1, y: 1, w: 4, h: 2 },
+    { id: 'widget-photo', type: 'widget', widgetType: 'photoWall', x: 1, y: 3, w: 2, h: 2 },
+
+    { id: 'app-qq', type: 'app', appId: 'qq', x: 3, y: 3, w: 1, h: 1 },
+    { id: 'app-sms', type: 'app', appId: 'sms', x: 4, y: 3, w: 1, h: 1 },
+    { id: 'app-weibo', type: 'app', appId: 'weibo', x: 3, y: 4, w: 1, h: 1 },
+    { id: 'app-forum', type: 'app', appId: 'forum', x: 4, y: 4, w: 1, h: 1 },
+
+    { id: 'app-music', type: 'app', appId: 'music', x: 1, y: 5, w: 1, h: 1 },
+    { id: 'app-theme', type: 'app', appId: 'theme', x: 2, y: 5, w: 1, h: 1 },
+    { id: 'app-settings', type: 'app', appId: 'settings', x: 3, y: 5, w: 1, h: 1 },
+    { id: 'app-widgetApp', type: 'app', appId: 'widgetApp', x: 4, y: 5, w: 1, h: 1 }
+  ]
 });
